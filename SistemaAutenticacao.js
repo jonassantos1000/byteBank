5ca88f10d5cb444f0f8cb9a9308f7daa2032c4f0
jonassantos1000 <jonasvale3@hotmail.com>
2022-04-21 16:30:29 -0300

    Correção do método autenticar da classe cliente.js

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -8,7 +8,9 @@ export class SistemaAutenticacao{
         return false;
     }
 
-    static ehAutentivavel(autenticavel){
-        
+    //Função que irá validar se o objeto recebido poderá ser considerado autenticavel.
+    static ehAutentivavel(autenticavel){ // função recebe o objeto que esta tentando autenticar(logar no sistema), ex: cliente, diretor, gerente etc..
+        return "autenticar" in autenticavel && // valida se o objeto possui o método autenticar na sua estrutura do model
+        autenticavel.autenticar instanceof Function // Aqui é especificado que deve procurar uma função chamada "autenticar" e não outro atributo do model do objeto
     }
 }
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { SistemaAutenticacao } from "./SistemaAutenticacao.js";
 
 const diretor = new Diretor("Rodrigo", 10000, 12345678900);
 const gerente = new Gerente("Ricardo", 5000, 12345678911);
-const cliente = new Cliente("teste",123456789);
+const cliente= new Cliente("lais",123456789,"321")
 
 diretor.cadastrarSenha("123456789");
 gerente.cadastrarSenha("123");
@@ -15,4 +15,9 @@ const gerenteEstaLogado= SistemaAutenticacao.login(gerente, "123");
 
 
 
-console.log(gerenteEstaLogado);
+const clienteEstaLogado = SistemaAutenticacao.login(cliente,"321")
+
+
+console.log("diretor ",diretorEstaLogado);
+console.log("gerente ",gerenteEstaLogado);
+console.log("cliente ", clienteEstaLogado);
